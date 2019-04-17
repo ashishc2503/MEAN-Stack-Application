d11d5cceb27aad93dc627d434cacde2567c45ac6
ashishc2503 <ahc32@njit.edu>
2019-04-16 20:37:52 -0400

    Section 6 completed

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,30 +1,33 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Post } from '../post.model';
-import { PostsService } from '../posts.service';
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { Subscription } from 'rxjs';
 
-@Component({
-  selector: 'app-post-list',
-  templateUrl: './post-list.component.html',
-  styleUrls: ['./post-list.component.css']
-})
+import { Post } from "../post.model";
+import { PostsService } from "../posts.service";
 
-export class PostListComponent implements OnInit, OnDestroy{
+@Component({
+  selector: "app-post-list",
+  templateUrl: "./post-list.component.html",
+  styleUrls: ["./post-list.component.css"]
+})
+export class PostListComponent implements OnInit, OnDestroy {
   // posts = [
-  //  {title: 'First Post', content: 'This is the First Post'},
-  //  {title: 'Second Post', content: 'This is the Second Post'},
-  //  {title: 'THird Post', content: 'This is the Third Post'}
+  //   { title: "First Post", content: "This is the first post's content" },
+  //   { title: "Second Post", content: "This is the second post's content" },
+  //   { title: "Third Post", content: "This is the third post's content" }
   // ];
   posts: Post[] = [];
+  isLoading = false;
   private postsSub: Subscription;
 
-  constructor( public postsService: PostsService) {}
+  constructor(public postsService: PostsService) {}
 
   ngOnInit() {
+    this.isLoading = true;
     this.postsService.getPosts();
     this.postsSub = this.postsService.getPostUpdateListener()
       .subscribe((posts: Post[]) => {
-          this.posts = posts;
+        this.isLoading = false;
+        this.posts = posts;
       });
   }
 
@@ -36,4 +39,3 @@ export class PostListComponent implements OnInit, OnDestroy{
     this.postsSub.unsubscribe();
   }
 }
-
